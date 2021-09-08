7f8f6d6155867f0e2094bb01979b6a669e0eb326
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2021-09-08 16:49:10 +0200

    Added in setup.py the configuration required by Sphinx.

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
     name='spas',
     version='0.0.1',
+    description='A python toolbox for acquisition of images based on the single-pixel framework.',
     author='Guilherme Beneti Martins',
+    url='https://github.com/openspyrit/spas',
+    long_description=readme,
+    long_description_content_type = "text/markdown",
+    install_requires=required
     packages=find_packages())
