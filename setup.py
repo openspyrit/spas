ff431399855cca0136ea533f374d8d5ededf7139
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2021-09-08 17:50:42 +0200

    Reoganized the list of dependencies from requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 with open('README.md') as f:
     readme = f.read()
 
@@ -14,5 +11,23 @@ setup(
     url='https://github.com/openspyrit/spas',
     long_description=readme,
     long_description_content_type = "text/markdown",
-    install_requires=required
+    install_requires=[
+        'ALP4lib @ git+https://github.com/guilhermebene/ALP4lib.git@7e35abf3a5c2e31652f7cfb2e4243b279b6a3a47',
+        'dataclasses-json (==0.5.2)',
+        'certifi',
+        'cycler',
+        'kiwisolver',
+        'matplotlib',
+        'numpy',
+        'msl-equipment @ git+https://github.com/MSLNZ/msl-equipment.git',
+        'Pillow',
+        'pyparsing',
+        'python-dateutil',
+        'six',
+        'tqdm (==4.60.0)',
+        'torch',
+        'torchvision',
+        'spyrit',
+        'wincertstore',
+    ]
     packages=find_packages())
